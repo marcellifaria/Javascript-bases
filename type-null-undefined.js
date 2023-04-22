73d0dbecc365c65a81f1ae92ffb1b57ebfdf7351
marcellifaria <113396665+marcellifaria@users.noreply.github.com>
2023-04-22 14:47:07 -0300

    alteração null -undefined

--- type-null-undefined.js
+++ type-null-undefined.js
@@ -5,3 +5,11 @@ if (input === null){
 }else{
     console.log(input);
 }
+
+/// exemplo 2 
+
+let inputNova = null;
+let input2;
+
+console.log(inputNova); // null
+console.log(input2); // undefined
