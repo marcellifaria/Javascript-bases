5ef98bf5a1e1d4e7ae8172b88525f85f22b489e2
marcellifaria <113396665+marcellifaria@users.noreply.github.com>
2023-04-22 12:12:04 -0300

    Tipos primitivos

---
+++ Type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'olá planeta';
+const senha = "senhaSupersegura456!";
+const stringDeNumeros ="345214";
+const citacao = ' A gata disse "miau"';
+
+console.log (citacao);
+
+// template string ou template literal - buscar
+
+
+// CONCATENAÇÃO (+)
+const nome = "Marcelli";
+const frase ="Meu nome é:";
+
+console.log (frase + nome);
+
---
+++ caracteres.js
@@ -0,0 +1,27 @@
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+/// Maiuscula e Minuscula
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+//quantas letras tem?
+const adivinhaQtdCaracteres = "qual é o nome da música";;
+console.log(adivinhaQtdCaracteres.length) 
+
+
+
+const quantasLetrasTem = "Inconstitucionalissimamente"
+console.log(quantasLetrasTem.length)
