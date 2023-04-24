443bfab3e9d5b335d711cbcd2758ccdd44beff18
marcellifaria <113396665+marcellifaria@users.noreply.github.com>
2023-04-23 21:22:56 -0300

    tipos verdadeiros e tipos falsos, null e undefined

---
+++ truthy-falsy.js
@@ -0,0 +1,35 @@
+// boolean
+
+const usuarioLogado = true
+const contaPaga = false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log(0 == false)
+console.log("" == false)
+
+console.log(1 == true)
+
+// null => representa o nada ou vazio
+
+let minhaVar;
+let varNull = null;
+
+console.log(typeof minhaVar) // retornará undefined
+console.log(typeof varNull) //retornará objeto por erro padrão do javascript
+
+
+
+
+
+
+
+
+
+
+
+
