cf8b58c228b826a3d88f6eb9ffb8ac9c822625f6
marcellifaria <113396665+marcellifaria@users.noreply.github.com>
2023-04-26 21:29:48 -0300

    mais sobre o console e como entender os erros

---
+++ console.js
@@ -0,0 +1,13 @@
+// log -> registro 
+let minhaVar = false;
+console.log(444)
+console.log("eu sou um texto")
+console.log(minhaVar)
+
+
+//tratamento de erro!
+console.error('deu erro!')
+
+//erro mostrando o stacktrace
+console.log("deu erro");
+console.error(new Error ("deu erro!"))
---
+++ erros.js
@@ -0,0 +1,5 @@
+// testando erros e como lê-los
+
+const numero = 0;
+const minhaVar = "Lanova"
+console.log(minhaVar)
