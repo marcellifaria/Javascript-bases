8bda0047907595d95ffefce600037835feddb531
MARCELLI PIRES FARIA <mpf@matera.com>
2023-05-15 22:00:44 -0300

    funções maie exemplos

--- funcoes.js
+++ funcoes.js
@@ -16,3 +16,27 @@ function soma (){
     return 2+2;
 }
 console.log(soma());
+
+
+function verificaNumero(numero) {
+    if (numero > 10) {
+         return 'número maior que 10';
+    } else {
+        return 'número não é maior que 10';
+    }
+}
+console.log(verificaNumero(9)) 
+
+//else if - em alguns casos é necessário mais de duas opções de fluxo para o código; 
+//para estes casos existe a cláusula else if:
+const num = 15;
+
+if (num >= 0 && num <= 10) {
+  console.log('número entre zero e dez');
+} else if (num > 10 && num <= 20) {
+  console.log('número entre dez e vinte');
+} else if (num > 20 && num <= 30) {
+  console.log('número entre vinte e trinta');
+} else {
+  console.log('outro número');
+}
